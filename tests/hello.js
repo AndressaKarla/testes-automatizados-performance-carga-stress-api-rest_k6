66d84a32eeaa0b00d1c054cca19aef661208012e
AndressaKarla <andressa.karla1@gmail.com>
2024-04-24 14:02:10 -0300

    Realizando algumas melhorias gitignore, environments, removendo ; desnecessário, etc

--- tests/hello.js
+++ tests/hello.js
@@ -1,12 +1,16 @@
 import http from 'k6/http'
 import { sleep } from 'k6'
 
+const environment = __ENV['ENVIRONMENT'] || 'local'
+const config = JSON.parse(open(`./support/environments/${environment}.json`))
+const baseUri = config.baseUri
+
 export const options = {
     vus: 10,
     duration: '30s',
 }
 
 export default function () {
-    http.get('http://localhost:3333')
-    sleep(1);
+    http.get(baseUri)
+    sleep(1)
 }
